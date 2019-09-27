da5ae6464fe2f7a0a1cead104ba32caccad0b590
Javier Mora <jinjuriki62@gmail.com>
2019-09-27 17:50:51 -0500

    Primer commit

---
+++ public/js/socket-nuevo-ticket.js
@@ -0,0 +1,26 @@
+//comando para establecet la comunicacion
+var socket = io();
+var label = $('#lblNuevoTicket');
+
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+
+socket.on('disconnect', function(){
+    console.log('Se perdio la conexion');
+});
+
+socket.on('estadoActual', function(resp){
+    console.log(resp);
+    label.text(resp.actual);
+});
+
+$('button').on('click', function() {
+    socket.emit('siguienteTicket', null, function(siguienteTicket){
+
+        label.text(siguienteTicket);
+
+    });
+});
